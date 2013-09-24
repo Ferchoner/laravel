efadc8c7ea63cab817d6dc79a4759f3ace4d2970
Ferchoner <rasoner@gmail.com>
2013-09-24 12:45:08 -0500

    Creating Project
    Adding all files

---
+++ app/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', array( 'as' => 'index', 'uses' => 'HomeController@showHome' ) );
+
+Route::post('/registrar', array('before' => 'validarInforRegistro', 'uses' => 'HomeController@registrarUsuario') );
+
+/* 
+ * Se cambio para usar layouts y controlladores
+ * a HomeController
+Route::get('/home', function()
+{
+	return View::make('principal');
+});
+*/
+Route::get('/home', function()
+{
+	return View::make('home');
+});
+
+Route::get('/registro', function()
+{
+	return View::make('registro');
+});
+
+Route::get('/login', function()
+{
+	return View::make('login');
+});
+
+Route::get('/prueba-form', function(){
+	return View::make('forma');
+});
