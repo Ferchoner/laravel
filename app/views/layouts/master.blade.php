cc7eaae43ea7a48e713570b093f6d7e75d9acd7c
Ferchoner <rasoner@gmail.com>
2014-01-13 16:46:54 -0600

    First Change

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class=" js no-touch svg inlinesvg svgclippaths no-ie8compat" lang="en">
+  <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    {{-- Set the viewport width to device width for mobile --}}
+    <meta name="viewport" content="width=device-width">
+    <title>Bienvenido a mi Primer Proyecto</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/foundation.css">
+
+    <script src="/js/vendor/custom.modernizr.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/foundation.min.js"></script>
+    <script src="/js/error-module.js"></script>
+    <script src="/js/principal.js"></script>
+    <script src="/js/vendor/zepto.js"></script>
+    <title>Usuarios</title> 
+  </head>
+  <body>
+    {{-- Header and Nav --}}
+    <div class="row">
+    <div class="large-3 columns">
+        <h1><img src="http://placehold.it/400x100&text=Logo" /></h1>
+    </div>
+    <div class="large-9 columns">
+      <ul class="right button-group">
+        <li>
+          <a id="signup" class="small button secondary">Registrate!</a>
+        </li>
+        <li>
+          <a id="login" class="small button secondary">Ingresar</a>
+        </li>        
+      </ul>
+    </div>
+    </div>
+    {{-- End Header and Nav --}}    
+
+    <div class="row">
+      {{-- Main Content Section --}}
+      {{-- This has been source ordered to come first in the markup (and on small devices) but to be to the right of the nav on larger screens --}}        
+      {{ $content }}
+      {{-- Nav Sidebar --}}
+      {{-- This is source ordered to be pulled to the left on larger screens --}}
+      <div class="large-3 pull-9 columns">          
+        <ul class="side-nav">
+          <li><a href="#">Section 1</a></li>
+          <li><a href="#">Section 2</a></li>
+          <li><a href="#">Section 3</a></li>
+          <li><a href="#">Section 4</a></li>
+          <li><a href="#">Section 5</a></li>
+          <li><a href="#">Section 6</a></li>
+        </ul>
+        
+        <p><img src="http://placehold.it/320x240&text=Ad" /></p>
+          
+      </div>
+    </div>  
+    {{-- Footer --}}
+    <footer class="row">
+      <div class="large-12 columns">
+        <hr />
+        <div class="row">
+          <div class="large-6 columns">
+            <p>&copy; Copyright no one at all. Go to town.</p>
+          </div>
+          <div class="large-6 columns">
+            <ul class="inline-list right">
+              <li><a href="#">Section 1</a></li>
+              <li><a href="#">Section 2</a></li>
+              <li><a href="#">Section 3</a></li>
+              <li><a href="#">Section 4</a></li>
+            </ul>
+          </div>
+        </div>
+      </div> 
+    </footer>
+  </body>
+</html>
---
+++ public/js/principal.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+
+	$('#login').click(function(e) {
+		$('#content').load('/login', function() {
+			loadLoginJS();
+		});		
+	});
+
+	$('#signup').click(function(e) {
+		$('#content').load('/registro', function() {
+			$(document).foundation();
+			loadRegistroJS();
+		});
+	});
+
+});
+
+var loadLoginJS = function() {
+	$('#aceptarLogin').unbind();
+	$('#aceptarLogin').click(function(e) {
+		$('input').each(function() {
+			// Validacion para inputs vacios, si quieres un input opcional solo agregale la clase canBN y se lo saltara
+			if ($(this).val() == '' && !$(this).hasClass('canBN')) {
+				showError($(this).attr('id'), 'Este campo es obligatorio');
+			};
+		});
+
+		// Validaciones hechas, falta codigo para hacer la llamada AJAX al controlador de login
+	});
+
+	$('input').unbind();
+	$('input').change(function(e) {
+		if ($(this).attr('id'))
+			hideError($(this).attr('id'));
+	});
+
+	$('#aHome').unbind();
+	$('#aHome').click(function(e) {
+		e.preventDefault();
+		$('#content').append('<div id="temp" style="visible:hidden"></div>');
+		$('#temp').load('/home #content', function() {
+			contenido = $('#temp #content').children();
+			$('#content').html(contenido);
+		});
+	});
+};
+
+var loadRegistroJS = function() {
+	$('#aceptarRegistro').unbind();
+	$('#aceptarRegistro').click(function(e) {
+		var hayErrores = false;
+		// Validacion para campos vacios en todos los inputs, si quieres un input opcional solo agregale la clase canBN y se lo saltara
+		$('input').each(function() {
+			if ($(this).val() == '' && !$(this).hasClass('canBN')) {
+				showError($(this).attr('id'), 'Este campo es obligatorio');
+				hayErrores = true;
+			};
+		});
+		//Validacion para selects de sexo
+		if ($('input[name=sexo]:checked').length <= 0) {
+			showErrorName('input', 'sexo');
+			hayErrores = true;
+		};
+		if (hayErrores) {
+			e.preventDefault();
+		}
+	});
+
+	$('input').unbind();
+	$('input').change(function(e) {
+		if ($(this).attr('name') == 'sexo')
+			hideErrorInput('input', $(this).attr('name'));
+		else
+			hideError($(this).attr('id'));
+	});
+
+	$('select').unbind();
+	$('select').change(function(e) {
+		hideErrorInput('select', $(this).attr('name'));
+	});
+
+	$('#aHome').unbind();
+	$('#aHome').click(function(e) {
+		e.preventDefault();
+		$('#content').append('<div id="temp" style="visible:hidden"></div>');
+		$('#temp').load('/home #content', function() {
+			contenido = $('#temp #content').children();
+			$('#content').html(contenido);
+		});
+	});
+};
