bec541ca08ac4341c5fb849aabd4baeb9232c4e7
Ferchoner <rasoner@gmail.com>
2014-03-05 18:01:49 -0600

    pimping maps

--- app/views/maps.blade.php
+++ app/views/maps.blade.php
@@ -1,9 +1,24 @@
 <style>
 	.mapa{ height: 600px; }
 </style>
-<div class="large-12 column">
-	<a id="aHome" class="right small secondary button">Regresar</a>
-</div>
-<div class="large-12 column">
-	<div id="map-canvas" class="mapa"></div>
-</div>
+{{ Form::open(array('method' => 'post', 'id'=>'maps-form', 'class' => 'custom')) }}
+	<fieldset>
+	  <legend>Busca tu dirección</legend>
+			 <div class="row collapse">
+				<div class="small-2 column">
+					{{ Form::label(null, 'Direcci&oacute;n', array('class' => 'prefix')) }}
+				</div>
+				<div class="small-5 column left">
+					{{ Form::text('address', null, array('id'=>'address', 'placeholder'=>'Dirección...')) }}
+				</div>
+				<div class="small-2 column">
+					{{ Form::submit('Buscar', array('class'=>'secondary button postfix', 'id'=>'searchStores', 'onClick'=>'event.preventDefault(); getStoresByAddress()')) }}
+				</div>
+				<div class="large-2 column">
+					<a id="aHome" class="right small secondary button">Regresar</a>
+				</div>
+			</div>
+	</fieldset>
+{{ Form::close() }}
+<div id="map-canvas" class="large-12 column mapa"></div>
+<script src="/js/maps.js" type="text/javascript" charset="utf-8"></script>
