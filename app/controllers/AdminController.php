1be2e4d2d0c3adf146764128462b5c5697bed698
Ferchoner <rasoner@gmail.com>
2014-06-02 13:34:02 -0500

    Some changes in PHP Storm from work

---
+++ app/controllers/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+class AdminController extends BaseController {
+	
+	public function indexAction(){
+		return TRUE;
+	}
+	
+	public function submitPost(){		
+		return TRUE;
+	}
+	
+	public function logoutAction(){		 
+        return TRUE;
+	}
+	
+	public function loginAction(){
+		return TRUE; 
+	}
+}
